ce8af1417f536fc8e0c2907dce3c8db8b1907c75
Christopher <136489810+Christopher7090@users.noreply.github.com>
2023-11-02 15:45:33 -0500

    11 COMMIT

--- PAlamacenado.sql
+++ PAlamacenado.sql
@@ -24,3 +24,42 @@ BEGIN
 
     COMMIT TRANSACTION;
 END;
+
+go
+create procedure listarEmpleadoId @id int
+as begin
+select * from Empleado as e inner join Usuario as u on u.IDEmpleado = e.ID inner join Cargo as c on c.ID = e.IDCargo where @id = e.ID order by e.ID;
+end
+go
+CREATE PROCEDURE editarEmpleado
+	@idempleado int,
+    @nombre VARCHAR(100),
+    @apellido VARCHAR(100),
+    @Dni char(8),
+    @Edad int,
+    @sexo char(1),
+	@idcargo int,
+	@usuario varchar(50),
+	@contrasena varchar(50),
+	@tipo varchar(50)
+AS
+BEGIN
+update  Empleado
+    set Nombres = @nombre, Apellido=@apellido, EdadEmpl=@Edad,SexoEmpl=@sexo,Dni=@Dni,IDCargo=@idcargo where ID=@idempleado;
+update Usuario set Usuario= @usuario, Contrasena=@contrasena, Tipocuenta=@tipo where IDEmpleado=@idempleado;
+end
+go
+CREATE PROCEDURE buscarEmpleado
+    @filtro varchar(50),
+	@nombre varchar(50)
+AS
+BEGIN
+if @filtro = 'Cargo'
+   Select e.ID,e.Nombres,e.Apellido,e.Dni,c.Cargo from Empleado as e  inner join Cargo as c on e.IDCargo = c.ID where c.Cargo LIKe '%' + @nombre + '%';
+else if @filtro = 'Nombres'
+   Select e.ID,e.Nombres,e.Apellido,e.Dni,c.Cargo from Empleado as e  inner join Cargo as c on e.IDCargo = c.ID where e.Nombres LIKe '%' + @nombre + '%';
+else if @filtro = 'Dni'
+   Select e.ID,e.Nombres,e.Apellido,e.Dni,c.Cargo from Empleado as e  inner join Cargo as c on e.IDCargo = c.ID where e.Dni LIKe '%' + @nombre + '%';
+END;
+
+
